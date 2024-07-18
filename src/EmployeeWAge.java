e2774314fc82e41019bbfad625c5177f2b36df7c
aamir105 <aamiranwer2018@gmail.com>
2024-07-18 15:20:34 +0530

    Added: UC4_Emp_Part_Full_Time_wage

--- src/EmployeeWAge.java
+++ src/EmployeeWAge.java
@@ -1,25 +1,26 @@
 public class EmployeeWAge {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HRS = 20;
+
     public static void main(String[] args) {
         System.out.println("HEELo");
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;
-        int EMP_RATE_PER_HOUR=20;
+
         int empHrs=0;
         int empWage=0;
 
-        double empCheck= Math.floor(Math.random()*10)%2;
-        if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
+        double empCheck= (int) Math.floor(Math.random()*10)%2;
+        switch (empWage){
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+            default:
+                empHrs = 0;
         }
-        else if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present.");
-            empHrs=8;
-        }
-        else {
-            System.out.println("Employee is Abscent.");
-            empHrs=0;
-        }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
+        empWage = empHrs * EMP_RATE_PER_HRS;
         System.out.println("Employee Wage : "+empWage);
     }
 }
