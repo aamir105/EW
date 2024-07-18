7e58544b780a7d357af2bb9d60b3b1311f18f465
aamir105 <aamiranwer2018@gmail.com>
2024-07-18 16:08:32 +0530

    ADDED:UC6_Total_Working_hours_Max_Days

--- src/EmployeeWAge.java
+++ src/EmployeeWAge.java
@@ -4,17 +4,19 @@ public class EmployeeWAge {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HRS = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTHS = 10;
 
     public static void main(String[] args) {
         System.out.println("HEELo");
 
         int empHrs=0;
-        int empWage=0;
-        int totalEmpWage = 0;
+        int totalEmpHrs=0;
+        int totalWorkinDays = 0;
 
-        for(int day = 0;day<NUM_OF_WORKING_DAYS;day++) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTHS && totalWorkinDays < NUM_OF_WORKING_DAYS) {
+            totalWorkinDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empWage) {
+            switch (empCheck) {
                 case IS_PART_TIME:
                     empHrs = 4;
                     break;
@@ -23,9 +25,10 @@ public class EmployeeWAge {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HRS;
-            System.out.println("Employee Wage : " + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day: " + totalWorkinDays + " Emp Hrs: " + empHrs);
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HRS;
         System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
