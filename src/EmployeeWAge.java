122202731955b907d926489c1e7a57f045fc7e7f
aamir105 <aamiranwer2018@gmail.com>
2024-07-18 14:46:35 +0530

    ADDED: UC2

--- src/EmployeeWAge.java
+++ src/EmployeeWAge.java
@@ -2,12 +2,20 @@ public class EmployeeWAge {
     public static void main(String[] args) {
         System.out.println("HEELo");
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHrs=0;
+        int empWage=0;
+
         double empCheck= Math.floor(Math.random()*10)%2;
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present.");
+            empHrs=8;
         }
         else {
             System.out.println("Employee is Abscent.");
+            empHrs=0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage : "+empWage);
     }
 }
